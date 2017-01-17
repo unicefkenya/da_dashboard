e5feee886010b644c80fb5d1bf21d31066b53b47
Shimanyi Valentino <shimanyi@sisitech.com>
2017-01-17 23:54:07 +0300

    default viewschools component files added

---
+++ app/src/app/schools/viewschools/viewschools.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewschools',
+  templateUrl: './viewschools.component.html',
+  styleUrls: ['./viewschools.component.scss']
+})
+export class ViewschoolsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
