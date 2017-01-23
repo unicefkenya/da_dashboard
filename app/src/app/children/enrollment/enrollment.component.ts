fa46c0d1ea85b6b94b4d32ec55f0e962cd7f2faf
Shimanyi Valentino <shimanyi@sisitech.com>
2017-01-23 07:50:23 +0300

    configured enrollment component

--- app/src/app/children/enrollment/enrollment.component.ts
+++ app/src/app/children/enrollment/enrollment.component.ts
@@ -15,22 +15,34 @@ export class EnrollmentComponent implements OnInit {
   public studentCount: any;
   public tmp: any;
   public students: any[];
+  public stats: any;
 
   private fetchEnrolled(){
     this.enrollmentService.fetchChildren().subscribe( data => {
-      console.log(data);
-      this.studentCount = data.length;
+      //console.log(data.students);
 
       let items = [];
-      for(let i = 0; i < data.length; i++){
+      for(let i = 0; i < data.students.length; i++){
           this.tmp = {};
-          this.tmp.firstname = data[i].student_name;
-          this.tmp.gender = data[i].gender;
-          this.tmp.lastdate = data[i].last_attendance;
+          this.tmp.studentname = data.students[i].student_name;
+          this.tmp.enrollmentdate = data.students[i].date_enrolled;
+          this.tmp.studentcode = data.students[i].emis_code;
+          this.tmp.gender = data.students[i].gender;
+          this.tmp.lastdate = data.students[i].last_attendance;
 
           items.push(this.tmp);
       }
       this.students = items;
+      console.log(this.students);
+
+      let statsTemp = {
+        "males" : data.males,
+        "females" : data.females,
+        "total" : data.total
+      };
+
+      this.stats = statsTemp;
+      console.log(this.stats);
 
     });
   }
