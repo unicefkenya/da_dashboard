9ff5c8d68b2c79a9ebd3e028e66e868cf131013d
Shimanyi Valentino <shimanyi@sisitech.com>
2017-03-01 02:27:31 +0300

    sample search > school page

---
+++ app/src/app/search/search.routing.ts
@@ -0,0 +1,8 @@
+import { Routes } from "@angular/router";
+
+import { SearchComponent } from './search.component';
+
+export const SearchRoutes: Routes = [{
+  path: '',
+  component: SearchComponent
+}];
