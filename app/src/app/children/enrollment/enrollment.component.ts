83bcc2637de9a8f625a9c5b8e3e95a06475e4a06
Norman Munge <normanmunge@gmail.com>
2017-07-04 13:53:11 +0300

    Enrollments tab working

--- app/src/app/children/enrollment/enrollment.component.ts
+++ app/src/app/children/enrollment/enrollment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { EnrollmentService } from './enrollment.service';
 
 @Component({
@@ -6,75 +7,205 @@ import { EnrollmentService } from './enrollment.service';
   templateUrl: './enrollment.component.html',
   styleUrls: ['./enrollment.component.scss'],
   providers: [EnrollmentService],
-
 })
+
 export class EnrollmentComponent implements OnInit {
 
-  constructor(private enrollmentService: EnrollmentService){
-    this.fetch((data) => {
-      this.rows = data;
-    });
-  }
+    loading:boolean;
+    dt:any;
+    children: any[] = this.rows;
+    selected: any[];
+    rows = [];
+    temp = [];
+    count: number = 0;
+    offset: number = 0;
+    limit: number = 100;
+    page:number=1
+    table = {
+      offset: 0
+    };
+    males:any;
+    females:any;
 
-  public temp = [];
-  public rows = [];
-  public studentCount: any;
-  public tmp: any;
-  public students: any[];
-  public total: any;
-  public males: any;
-  public females: any;
+    partnerId:number;
 
-  private fetchEnrolled(){
-    this.enrollmentService.fetchChildren().subscribe( data => {
+    columns = [
+      { name: 'Emiscode' },
+      { name: 'Name', filtering:{filterString: '', placeholder: 'Filter by name'} },
+      { name: 'Gender' },
+      { name: 'Attendance' },
+      { name: 'Class' },
 
-      let items = [];
-      for(let i = 0; i < data.students.length; i++){
-          this.tmp = {};
-          this.tmp.studentname = data.students[i].student_name;
-          this.tmp.enrollmentdate = data.students[i].date_enrolled;
-          this.tmp.studentcode = data.students[i].emis_code;
-          this.tmp.gender = data.students[i].gender;
-          this.tmp.lastdate = data.students[i].last_attendance;
+    ];
 
-          items.push(this.tmp);
+  constructor(private enrollmentService: EnrollmentService, private router: Router){}
+
+  //admin
+  fetchChildren(offset,limit): void {
+    this.enrollmentService.fetchChildren(this.page).subscribe(data => {
+      //start and end for pagination
+      const start = offset * limit;
+      const end = start + limit;
+       this.count =data.count
+      data = data.results;
+      this.loading = false;
+
+      let childs =[]
+      let rows=[]
+      //  this.count = data.length;
+      for (let i = 0;i < data.length;i++){
+        this.dt = {}
+        this.dt.emiscode=data[i].emis_code
+        this.dt.name=data[i].student_name
+        this.dt.gender=data[i].gender
+        this.dt.attendance=data[i].last_attendance
+        this.dt.class=data[i].class_name
+        this.dt.id = data[i].id
+        childs.push(this.dt)
       }
+      //cache our data
+      //this.temp = childs;
+      let row=[...rows]
+      this.temp=[...childs];
+      let j=0
+      for (let i = start; i < end; i++) {
+        row[i] = childs[j];
+        j++;
+      }
+      //this.temp=row
+      this.children=row;
 
-      this.students = items;
+      this.selected = [];
 
-      this.males = data.males;
-      this.females = data.females;
-      this.total = data.total;
+      //console.log('Page Results',this.children,this.count, start, end);
 
     });
   }
 
-  //filtering the table
-   updateFilter(event) {
-     let val = event.target.value;
-     // filter our data
-     let temp = this.temp.filter(function(d) {
-       return d.name.toLowerCase().indexOf(val) !== -1 || !val;
-     });
-     // update the rows
-     this.rows = temp;
-   }
 
-   //sorting the table
-    fetch(cb) {
-       const req = new XMLHttpRequest();
-       req.open('GET', `assets/data/company.json`);
+    //individual partners
+    fetchPartnerChildren(id,offset,limit): void {
+      this.enrollmentService.fetchPartnerChildren(id,this.page).subscribe(data => {
+        //start and end for pagination
+        const start = offset * limit;
+        const end = start + limit;
+         this.count =data.count
+        data = data.results;
+        this.loading = false;
 
-       req.onload = () => {
-         let data = JSON.parse(req.response);
-         cb(data);
-       };
+        let childs =[]
+        let rows=[]
+        //  this.count = data.length;
+        for (let i = 0;i < data.length;i++){
+          this.dt = {}
+          this.dt.emiscode=data[i].emis_code
+          this.dt.name=data[i].student_name
+          this.dt.gender=data[i].gender
+          this.dt.attendance=data[i].last_attendance
+          this.dt.class=data[i].class_name
+          this.dt.id = data[i].id
+          childs.push(this.dt)
+        }
+        //cache our data
+        //this.temp = childs;
+        let row=[...rows]
+        this.temp=[...childs];
+        let j=0
+        for (let i = start; i < end; i++) {
+          row[i] = childs[j];
+          j++;
+        }
+        //this.temp=row
+        this.children=row;
 
-       req.send();
+        this.selected = [];
+
+        //console.log('Page Results',this.children,this.count, start, end);
+
+      });
+    }
+
+    fetchBoyChildTotal(){
+      this.enrollmentService.fetchBoyChildTotal().subscribe(data => {
+        this.males = data.count;
+      });
+    }
+
+    fetchPartnerBoyChildTotal(id){
+      this.enrollmentService.fetchPartnerBoyChildTotal(id).subscribe(data => {
+        this.males = data.count;
+      });
+    }
+
+    fetchGirlChildTotal(){
+      this.enrollmentService.fetchGirlChildTotal().subscribe(data => {
+          this.females = data.count;
+      });
+    }
+
+    fetchPartnerGirlChildTotal(id){
+      this.enrollmentService.fetchPartnerGirlChildTotal(id).subscribe(data => {
+        this.females = data.count;
+      });
+    }
+
+    onSelect({ selected }) {
+     //console.log('Select Event', selected, this.selected,this.selected[0].id);
+     localStorage.setItem('childId', this.selected[0].id);
+     this.getChildId(this.selected[0].id);
+     //this.router.navigate(['/children/child', this.selected[0].id]);
      }
 
+     private getChildId(id){
 
-  ngOnInit(): void {
-    this.fetchEnrolled();
-  }
+       this.router.navigate(['/children/child', id]);
+
+     }
+
+     onActivate(event) {
+       //console.log('Activate Event', event);
+     }
+
+    updateFilter(event) {
+      const val = event.target.value.toLowerCase();
+
+      // filter our data
+      const temp = this.temp.filter(function(d) {
+        return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+      });
+      // update the rows
+        this.children = temp;
+        // Whenever the filter changes, always go back to the first page
+        this.table.offset = this.page;
+
+      console.log('Filter event', event);
+    }
+
+
+
+    onPage(event) {
+      console.log(event.offset);
+      this.page=event.offset+1
+      if(this.partnerId){
+        this.fetchPartnerChildren(this.partnerId,event.offset, event.limit);
+      }else{
+        this.fetchChildren(event.offset, event.limit);
+      }
+    }
+
+    ngOnInit(): void {
+      this.loading = true;
+      this.partnerId = JSON.parse(localStorage.getItem("partnerId"));
+
+      if(this.partnerId){
+        this.fetchPartnerChildren(this.partnerId,this.offset, this.limit);
+        this.fetchPartnerBoyChildTotal(this.partnerId);
+        this.fetchPartnerGirlChildTotal(this.partnerId);
+      }else{
+        this.fetchChildren(this.offset, this.limit);
+        this.fetchBoyChildTotal();
+        this.fetchGirlChildTotal();
+      }
+
+    }
 }
--- app/src/app/children/enrollment/enrollment.service.ts
+++ app/src/app/children/enrollment/enrollment.service.ts
@@ -2,13 +2,44 @@ import { Injectable } from '@angular/core';
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import 'rxjs/Rx';
 import { Observable } from 'rxjs/Rx';
+import { BaseUrl} from '../../baseurl';
 
 @Injectable()
 export class EnrollmentService {
+  private baseApiUrl = BaseUrl.base_api_url;
   constructor( private http: Http){}
 
-  fetchChildren(){
-    return this.http.get('http://uoosc.cloudapp.net/api/students/enrolls')
+  fetchChildren(page){
+    return this.http.get(this.baseApiUrl+'api/students/enrolls?is_oosc=true&page='+page)
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  fetchPartnerChildren(id,page){
+    return this.http.get(this.baseApiUrl+'api/students/enrolls?is_oosc=true&page='+page+'&partner='+id)
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  fetchBoyChildTotal(){
+    return this.http.get(this.baseApiUrl+'api/students/enrolls?is_oosc=true&gender=M')
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
+  fetchPartnerBoyChildTotal(id){
+    return this.http.get(this.baseApiUrl+'api/students/enrolls?is_oosc=true&gender=M&partner='+id)
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  fetchGirlChildTotal(){
+      return this.http.get(this.baseApiUrl+'api/students/enrolls?is_oosc=true&gender=F')
+        .map((response: Response) => response.json())
+        .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+    }
+
+  fetchPartnerGirlChildTotal(id){
+    return this.http.get(this.baseApiUrl+'api/students/enrolls?is_oosc=true&gender=F&partner='+id)
       .map((response: Response) => response.json())
       .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
   }
