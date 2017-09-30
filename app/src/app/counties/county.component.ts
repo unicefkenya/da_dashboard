78a178b29d5977aaaad4051f2a6622fe3b7496a9
Norman Munge <normanmunge@gmail.com>
2017-10-01 01:18:04 +0300

    Getting counties from api and added google map

--- app/src/app/counties/county.component.ts
+++ app/src/app/counties/county.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { CountyService} from './county.service';
-//import { County } from './county';
 
+export class Marker {
+  constructor(
+    public lat:any,
+    public lng:any,
+    public draggable:any) { }
+}
 
 
 @Component({
@@ -13,5 +18,22 @@ import { CountyService} from './county.service';
 
 })
 export class CountyComponent implements OnInit {
+
+lat: number = 0.1768696;
+lng: number = 37.9083264;
+zoom: number = 11;
+draggable: boolean = true;
+
   ngOnInit(){}
+
+    markers: Marker[]=[{
+      lat: -32.9477132,
+      lng: -60.630465800000025,
+      draggable: false
+    },{
+      lat: -32.9477132,
+      lng: -60.630465800000025,
+      draggable: false
+    }
+  ];
 }
--- app/src/app/counties/county.module.ts
+++ app/src/app/counties/county.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AgmCoreModule } from 'angular2-google-maps/core';
 import { MdIconModule, MdCardModule, MdInputModule, MdButtonModule, MdToolbarModule, MdTabsModule, MdListModule, MdSlideToggleModule, MdSelectModule } from "@angular/material";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from "@angular/flex-layout";
@@ -10,9 +11,12 @@ import { CountyRoutes } from './county.routing';
 import { CountyComponent } from './county.component';
 
 
+const googleMapsCore = AgmCoreModule.forRoot({apiKey: 'AIzaSyAyMZ31YQXhc2OhbGPIKNPTEDZ3YoGknuU',});
+
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(CountyRoutes), MdIconModule, MdCardModule, MdInputModule, MdButtonModule, MdToolbarModule, MdTabsModule, MdListModule, MdSlideToggleModule, MdSelectModule, FlexLayoutModule, FormsModule, ReactiveFormsModule,NgxDatatableModule],
+  imports: [CommonModule, RouterModule.forChild(CountyRoutes), googleMapsCore,MdIconModule, MdCardModule, MdInputModule, MdButtonModule, MdToolbarModule, MdTabsModule, MdListModule, MdSlideToggleModule, MdSelectModule, FlexLayoutModule, FormsModule, ReactiveFormsModule,NgxDatatableModule],
   declarations: [ CountyComponent]
 })
+
 
 export class CountyModule {}
