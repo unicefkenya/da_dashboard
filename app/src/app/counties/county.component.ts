0ae258ffabf13586b331d330ab2ede7eb60430af
Norman Munge <normanmunge@gmail.com>
2017-10-03 21:02:56 +0300

    Added example geojson

--- app/src/app/counties/county.component.ts
+++ app/src/app/counties/county.component.ts
@@ -36,27 +36,34 @@ markers: Marker[]=[{
 }
 ];
 
-counties = [
-  {name: 'nairobi',lat:1.2921,lng:36.8219,},
-  {name: 'kisumu',lat:0.0917,lng:34.7680,}
-]
-
-geoJsonObject: Object = [
-  {lat:1.2921,lng:36.8219,},
-  {lat:0.0917,lng:34.7680,}
-];
+selections =[{select:'Total Children'},{select: 'Newly Enrolled'},{select: 'Dropouts'}];
   //http://technobytz.com/mapping-angular-google-maps-api-geojson.html
 
   constructor(private countyService: CountyService){}
 
-  ngOnInit(){
-    this.getCountiesData();
-  }
+  ngOnInit(){}
   private map:any;
-
+  countyName:any;
+  kaunti:any;
   ngAfterViewInit(){
+      this.getCountiesData();
+
+      this.getMapData((data) => {
+        console.log(data, 'kenyancountiesjson')
+        let properties = data.features
+        let kaunt = []
+        for (let i=0;i<properties.length; i++){
+          this.kaunti ={}
+          this.kaunti.countyName = properties[i].properties.COUNTY;
+          kaunt.push(this.kaunti);
+        }
+      });
+
+      var censusMin = Number.MAX_VALUE, censusMax = -Number.MAX_VALUE;
+
 
       function initMap() {
+
         this.map = new google.maps.Map(document.getElementById('map'), {
           zoom: 6,
           center:{lat: 0.176869, lng: 37.9083264},
@@ -70,6 +77,17 @@ geoJsonObject: Object = [
           strokeColor: '#521DB7',
           strokeWeight: 2
         });
+
+        this.map.data.setStyle(function(feature) {
+         var color = '#521DB7';
+
+         return  ({
+            fillColor: color,
+            strokeColor: color,
+            strokeWeight: 2
+          });
+       });
+
         this.map.data.addListener('mouseover', function(event) {
         this.map.data.revertStyle();
         this.map.data.overrideStyle(event.feature, {strokeWeight: 8});
@@ -78,27 +96,69 @@ geoJsonObject: Object = [
         this.map.data.addListener('mouseout', function(event) {
         this.map.data.revertStyle();
         });
+
+        var selectBox = document.getElementById('census-variable');
+        google.maps.event.addDomListener(selectBox, 'change', function() {
+          clearCensusData();
+        //  loadCensusData(selectBox.options[selectBox.selectedIndex].value);
+        });
+
       }
       google.maps.event.addDomListener(window, "load", initMap);
+
+      /** Removes census data from each shape on the map and resets the UI. */
+      function clearCensusData() {
+        censusMin = Number.MAX_VALUE;
+        censusMax = -Number.MAX_VALUE;
+        this.map.data.forEach(function(row) {
+          row.setProperty('census_variable', undefined);
+        });
+        document.getElementById('data-box').style.display = 'none';
+        document.getElementById('data-caret').style.display = 'none';
+      }
+
+
   }
 
 ct:any;
+count =[];
   getCountiesData(){
     this.countyService.getCountiesData().subscribe(data=>{
+      console.log(data, 'ooscapi');
       data = data.results;
-      let counties = []
       for(let a=0; a<data.length; a++){
         this.ct = {}
         this.ct.name = data[a].value;
         this.ct.enrolled = data[a].enrolled_females + data[a].enrolled_males;
         this.ct.total = data[a].old_females + data[a].old_males;
         this.ct.dropouts = data[a].dropout_total;
-        counties.push(this.ct);
-      }console.log(counties, 'hizi county');
-
+        this.count.push(this.ct);
+      }
     }),error =>{
       console.log(error, 'aiyyayay');
     }
+  }
+kaunt = []
+  getData(){
+    this.getMapData((data) => {
+      console.log(data, 'kenyancountiesjson')
+      let properties = data.features
+
+      for (let i=0;i<properties.length; i++){
+        this.kaunti ={}
+        this.kaunti.countyName = properties[i].properties.COUNTY;
+        this.kaunt.push(this.kaunti);
+      }
+      return this.kaunt;
+    });
+  }
+  getMapData(data){
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/data/kenyancounties.json`);
+    req.onload = () => {
+      data(JSON.parse(req.response));
+    };
+    req.send();
   }
 
   styleFunc(feature){
